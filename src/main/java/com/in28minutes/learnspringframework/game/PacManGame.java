30df2a16d72c6bd2fe74012ac4691b699dadffe9
jeong <chung_junhuck@infonic.co.jp>
2023-12-02 22:20:23 +0900

    spring: injection dependency and bean

---
+++ src/main/java/com/in28minutes/learnspringframework/game/PacManGame.java
@@ -0,0 +1,25 @@
+package com.in28minutes.learnspringframework.game;
+
+public class PacManGame implements GamingConsole {
+
+	@Override
+	public void up() {
+		System.out.println("mogumogu up");
+	}
+
+	@Override
+	public void down() {
+		System.out.println("mogumogu down");
+	}
+
+	@Override
+	public void left() {
+		System.out.println("mogumogu left");
+	}
+
+	@Override
+	public void right() {
+		System.out.println("mogumogu right");
+	}
+
+}
